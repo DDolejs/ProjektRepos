c07de2e88d7a089206457ff4e1674503a28d3a3b
Dolejš Daniel <dolejs.daniel.2017@skola.ssps.cz>
2020-06-01 15:50:41 +0200

    přidání test classy

---
+++ FoodGenerator_test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FoodGenerator_test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FoodGenerator_test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2f224f03-72c6-43ab-8922-0f598172365a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FoodGenerator_test/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FoodGenerator_test
+{
+    [TestClass]
+    
+    public class UnitTest1
+    {
+        
+        public UnitTest1() { }
+        [TestMethod]
+        public void GenerateTest()
+        {
+                
+        }
+    }
+}
